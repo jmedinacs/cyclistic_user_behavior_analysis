20b787b7654e1b4cd285e56fe19fc723ba48ac8b
John Medina <jmedinacs715@gmail.com>
2025-02-28 00:01:22 -0800

    Added cleaning script to compute ride duration and add a column. This also flags ride durations that are zero and negative.

---
+++ scripts/CLEANING_ride_duration_column.R
@@ -0,0 +1,37 @@
+# CLEANING_ride_duration_column
+# This script will compute the ride duration based on started_at and 
+# ended_at POSIXct values and add a column. 
+
+# Load configuration settings and variables
+source("config.R")
+
+# Load the most current version of the cleaned_data_rds to ensure accuracy
+cleaned_data <- readRDS(cleaned_data_rds)
+
+# Function that calculates the ride duration in terms of minutes
+calculate_ride_duration <- function(df){
+  df$ride_duration <- as.numeric(difftime(df$ended_at, df$started_at,
+                                          units="mins"))
+  
+  # Flag invalid duration of negative or zero minutes
+  df$invalid_duration <- df$ride_duration <=0
+  
+  # Count the number of flagged rows
+  num_invalid <- sum(df$invalid_duration)
+  
+  cat("Found", num_invalid, "rides with invalid durations(<=0 minutes.\n")
+  cat("invalid rides flagged but not removed. \n")
+  
+  return(df)
+}
+
+# Use the function
+cleaned_dataset <- calculate_ride_duration(cleaned_data)
+
+# Save an RDS version using config.R variable
+saveRDS(cleaned_dataset, file = cleaned_data_file)
+
+# Save a CSV version dynamically
+write.csv(combined_dataset, 
+          file = sub(".rds", ".csv", cleaned_data_file), 
+          row.names = FALSE)
