dc4ff3328a8c2b9de6ea1c270ba34ceeb68f67cd
John Medina <jmedinacs715@gmail.com>
2025-03-06 22:23:01 -0800

    Created script 10 that computs and creates a day of week column in the dataset using the started_at column.

---
+++ scripts/10_eda_day_of_week_column.R
@@ -0,0 +1,21 @@
+# 10_eda_day_of_week_column
+# This script identifies the day of the week each ride started_at occurs
+# and create a column for it. This will be used for analysis that involves
+# identifying peak days of the week.
+
+# Load configuration and variables
+source("config.R")
+
+# Trial
+sample_date <- as.POSIXct("2024-02-25 14:30:00")
+wday(sample_date, label = TRUE, abbr = FALSE)
+
+cleaned_data <- cleaned_data %>% 
+  mutate(day_of_week = wday(started_at, label = TRUE, abbr = FALSE))
+
+# Verify that the column was added
+str(cleaned_data)
+
+# Save the updated dataset
+saveRDS(cleaned_data, file = cleaned_data_rds)
+write.csv(cleaned_data, file = cleaned_data_csv, row.names = FALSE)
